c02c264ae640731c5d01450c602febe82664a888
ercingmec <erickreyescornejo9968@gmail.com>
2025-04-02 08:26:24 -0600

    ejercicio 2 p8

---
+++ Eejericio2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+    int n, i;
+    printf ("ingresa un nùmero natural\n");
+    scanf("%d", &n);
+    i= 1;
+    do {
+        printf ("%d", i);
+        i++;
+        {}
+    }  while (i<=n);
+    return 0;
+} 
+
